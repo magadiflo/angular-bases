47dd66a752b0c62fa513fd79afc566f6ef6c8125
Martín <magadiflo@gmail.com>
2022-06-30 23:59:19 -0500

    Módulos - Segunda parte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-import { ContadorComponent } from './contador/contador.component';
-
 import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
   ],
   imports: [
     BrowserModule,
     HeroesModule,
+    ContadorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -13,9 +13,29 @@ import { ListadoComponent } from "./listado/listado.component";
         ListadoComponent,
     ],
     imports: [
-        CommonModule,
+        CommonModule, //*Ofrece las directivas como el *ngIf, *ngFor...
     ],
 })
 export class HeroesModule {
 
 }
+
+/**
+ * *IMPORTANTE:
+ * *Cuando trabajamos en la aplicación teniendo únicamente 
+ * *al app.module.ts no requeríamos importar el CommonModule
+ * *para hacer uso de las directivas *ngIf, *ngFor, etc...
+ * *Pero ahora que vamos creando nuestros propios módulos, 
+ * *necesitamos importar el CommonModule para hacer uso de esas
+ * *directivas. ¿Por qué?
+ * 
+ * *RESPUESTA:
+ * *De esta manera funciona angular, BrowserModule se encarga de proveer 
+ * *los servicios esenciales para desplegar y ejecutar la app en el navegador, 
+ * *adicionalmente re-exporta CommonModule desde @angular/common, 
+ * *lo que significa que los componentes del módulo AppModule también tienen 
+ * *acceso a las directivas de Angular que toda aplicación necesita, 
+ * *como NgIf y NgFor.
+ * *Esto significa que para el resto de módulos que no sean appModule, 
+ * *es necesario el CommonModule.
+ */
