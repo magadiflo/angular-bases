b1bdfa0cd6b999830f82a34dbd749b59d3bc4fa2
Martín <magadiflo@gmail.com>
2022-06-30 22:40:48 -0500

    Crear componentes de forma automática

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 
 import { ContadorComponent } from './contador/contador.component';
 import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { ListadoComponent } from './heroes/listado/listado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContadorComponent,
     HeroeComponent,
+    ListadoComponent,
   ],
   imports: [
     BrowserModule
