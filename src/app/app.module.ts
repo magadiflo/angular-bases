5138711a8a40fc9f56601c4f0d9fc0c352c2bd57
Martín <magadiflo@gmail.com>
2022-06-30 22:01:39 -0500

    Componente de Heroe y separación de directorios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-import { ContadorComponent } from './contador.component';
+import { ContadorComponent } from './contador/contador.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContadorComponent,
+    HeroeComponent,
   ],
   imports: [
     BrowserModule
