bbf1ec478da7563a0d76c2b12a1ba6213f88b409
Martín <magadiflo@gmail.com>
2022-07-05 23:21:44 -0500

    Mostrar listado de personajes

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -11,14 +11,23 @@ interface Personaje {
 })
 export class MainPageComponent {
 
-  nuevo: Personaje = {
-    nombre: 'Trunks',
-    poder: 14000,
-  }
+  nuevo: Personaje = this.inicializaPersonaje();
+
+  personajes: Personaje[] = [
+    { nombre: 'Bulma', poder: 300 },
+    { nombre: 'Maestro Roshi', poder: 800 },
+  ];
 
 
   agregar(): void {
-    console.log(this.nuevo); 
+    if (this.nuevo.nombre.trim().length === 0) return;
+    console.log(this.nuevo);
+    this.personajes.push(this.nuevo);
+    this.nuevo = this.inicializaPersonaje();
+  }
+
+  private inicializaPersonaje(): Personaje {
+    return { nombre: '', poder: 0 };
   }
 
 }
