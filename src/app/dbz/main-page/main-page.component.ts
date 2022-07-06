9640c06b014686edf9b44f69e3a6a4c8591c25d7
Martín <magadiflo@gmail.com>
2022-07-05 23:07:05 -0500

    ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
 
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 14000,
+  }
+
 
   agregar(): void {
-    console.log('agrega....'); 
+    console.log(this.nuevo); 
   }
 
 }
