13d8b25b847aa54d669841fa8914a076c3926e03
Martín <magadiflo@gmail.com>
2022-07-05 22:44:48 -0500

    FormsModule

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { MainPageComponent } from './main-page/main-page.component';
 
@@ -14,6 +15,7 @@ import { MainPageComponent } from './main-page/main-page.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
   ],
 })
 export class DbzModule { }
